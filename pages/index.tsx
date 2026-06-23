fa5cff563dc4f3de29ad99c87b37682fd9ab323b
corentin06062003 <129956412+corentingesse@users.noreply.github.com>
2026-06-23 09:07:31 +0200

    exercice 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+"use client";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 style={{ fontSize: "4rem" }}>HomePage</h1>
+      <span className="material-symbols-outlined">
+      keep
+      </span>
+      <span className="material-symbols-outlined">
+      keep
+      </span>
+    </div>
+  );
 }
